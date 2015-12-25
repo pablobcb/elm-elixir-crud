02584fad41604ba2fcadd98062a71534a4a7a4f4
Pablo Botelho <pablo.bcb@gmail.com>
2015-12-25 05:43:50 +0000

    migration working

--- backend/config/dev.exs
+++ backend/config/dev.exs
@@ -36,8 +36,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :backend, Backend.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "backend_dev",
+  password: "backend_dev",
   database: "backend_dev",
   hostname: "localhost",
   pool_size: 10
---
+++ backend/priv/repo/migrations/20151225053434_create_user.exs
@@ -0,0 +1,14 @@
+defmodule Backend.Repo.Migrations.CreateUser do
+  use Ecto.Migration
+
+  def change do
+    create_if_not_exists table(:users) do
+      add :username,      :string, size: 200
+      add :email,         :string, size: 200
+      add :hash,          :string, size: 130
+      add :recovery_hash, :string, size: 130
+      
+      timestamps
+    end
+  end
+end
